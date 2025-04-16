b90072e3c703d7cba33c00c82f52e79cfe8a0b54
hualm <hu.almohseni@gmail.com>
2025-04-16 14:46:31 -0400

    updated the port number

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log("Server Started");
